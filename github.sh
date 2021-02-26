07fa9e47cc1ceada7b6044b0bcfd73e0234fc53e
shadyskies <mehta.naman328@gmail.com>
2021-02-26 22:10:11 +0530

    tmp

--- github.sh
+++ github.sh
@@ -2,5 +2,6 @@
 
 while IFS="\n" read a b; do
     git status
-    git commit -m "$1" & git push
+    git commit -m "$1" 
+    git push "https://$a:$b@100Days.biz/file.git" --all
 done < github_cred.txt
