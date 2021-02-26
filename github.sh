f802164cc7f78daecd204a7e7f77d89455fced4e
shadyskies <mehta.naman328@gmail.com>
2021-02-26 22:14:06 +0530

    tmp

--- github.sh
+++ github.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-while IFS="\n" read a b; do
-    git status
-    git commit -m "$1" 
-    git push "https://$a:$b@100Days.biz/file.git" --all
-done < github_cred.txt
+# username=''
+# PAT=''
+# while IFS="\n" read a b; do
+    
+# done < github_cred.txt
+#     git status
+#     git commit -m "$1" 
+#     git push "https://$a:$b@100Days.git/file.git" --all
+
+git commit -m "$1"
+git push origin master
